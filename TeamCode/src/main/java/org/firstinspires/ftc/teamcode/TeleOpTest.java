45bf6e08eda7849d4a2f8dcff320150385f85df0
Owen-Pryga <58037675+Owen-Pryga@users.noreply.github.com>
2022-01-19 16:31:48 -0600

    add some features that broken but added 3 pre programmed levels bumpers switch between the 3 levels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -10,7 +10,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @TeleOp(name = "TestOp")
 public class TeleOpTest extends LinearOpMode{
     private boolean buttonPressed;
+    private boolean setLevel = false;
+    private int lvl;
+    private int liftTolerance = 100;
     RobotDrive robot = new RobotDrive();
+    private int levels[] = {-500,-1800,-3400};
+
     public void runOpMode()
     {
         waitForStart();
@@ -21,24 +26,45 @@ public class TeleOpTest extends LinearOpMode{
         //drop the arm uppon initalization dont got to do it here just testing do it in auto
         robot.dropArm.setPosition(1);
 
-        while (opModeIsActive())
-    {
+        while (opModeIsActive()) {
 
 
-        double forward = gamepad1.left_stick_y * -1; //the gamepad is reversed thus the -1
-        double strafe = gamepad1.left_stick_x;
-        double rotate = gamepad1.right_stick_x * robot.motorPower;
+            double forward = gamepad1.left_stick_y * -1; //the gamepad is reversed thus the -1
+            double strafe = gamepad1.left_stick_x;
+            double rotate = gamepad1.right_stick_x * robot.motorPower;
 
-        robot.mixDrive(forward, strafe, rotate);
+            robot.mixDrive(forward, strafe, rotate);
 
-        //up is left trigger aka moving the motor in the negative direction
-        //set the power and direction of lift motor by subtracting the two values also check to see if robot is at top or bottom
-        if(robot.liftMotor.getCurrentPosition() <= 4700)
-            robot.liftMotor.setPower(gamepad2.right_trigger);
-        else if(robot.dist.getDistance(DistanceUnit.INCH) <= 1.8)
-            robot.liftMotor.setPower(-gamepad2.left_trigger);
-        else if(!(robot.liftMotor.getCurrentPosition() <= -4700) || (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8))
-            robot.liftMotor.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+            //up is left trigger aka moving the motor in the negative direction
+            //set the power and direction of lift motor by subtracting the two values also check to see if robot is at top or bottom
+            if (robot.liftMotor.getCurrentPosition() <= -4700 && gamepad2.right_trigger >0.1) {
+                robot.liftMotor.setPower(gamepad2.right_trigger);
+                setLevel = false;
+            } else if (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8 && gamepad2.left_trigger >0.1) {
+                robot.liftMotor.setPower(-gamepad2.left_trigger);
+                setLevel = false;
+            } else if ((!(robot.liftMotor.getCurrentPosition() <= -4700) || (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8))
+                    && gamepad2.left_trigger >0.1 && gamepad2.right_trigger >0.1){
+                robot.liftMotor.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+                setLevel = false;
+            }
+
+        if(gamepad2.left_bumper && gamepad2.right_bumper){}
+        else if(gamepad2.left_bumper && lvl < 2) {
+            setLevel = true;
+            lvl++;
+        }else if(gamepad2.right_bumper && lvl > 0) {
+            setLevel = true;
+            lvl--;
+        }
+
+        if(setLevel)
+            if(robot.liftMotor.getCurrentPosition() < levels[lvl] && robot.liftMotor.getCurrentPosition() > levels[lvl] - liftTolerance)
+                robot.liftMotor.setPower(0);
+            else if(robot.liftMotor.getCurrentPosition() < levels[lvl] - liftTolerance)
+                robot.liftMotor.setPower(1);
+            else if(robot.liftMotor.getCurrentPosition() > levels[lvl])
+                robot.liftMotor.setPower(-1);
 
 
         //if the robot arm is at the base position reset arm encoder to ensure accuracy
@@ -56,7 +82,7 @@ public class TeleOpTest extends LinearOpMode{
 
         //turn on the duck wheel when gamepad2's a button is pressed
         if(gamepad2.a)
-            robot.duckMotor.setPower(0.5);
+            robot.duckMotor.setPower(-0.75);
         else
             robot.duckMotor.setPower(0);
 
@@ -73,7 +99,8 @@ public class TeleOpTest extends LinearOpMode{
 
 
 
-
+        telemetry.addData("Lift yn ",setLevel);
+        telemetry.addData("liftLvl: ", lvl);
         telemetry.addData("Left Rear motor power: ",robot.motors[0].getPower());
         telemetry.addData("GrabberServo: ", robot.Grabber.getPosition());
         telemetry.addData("Distance: ","%.3f",((DistanceSensor) robot.dist).getDistance(DistanceUnit.INCH));
