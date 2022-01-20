f6a0d1cfdddf9123255b4369fd9f63d9135a6ccd
Owen-Pryga <58037675+Owen-Pryga@users.noreply.github.com>
2022-01-20 06:41:31 -0600

    worked on auto lvl set
    changed some things so lvl set was a toggle and not a like a cruise cancel because that is difficult

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -11,7 +11,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class TeleOpTest extends LinearOpMode{
     private boolean buttonPressed;
     private boolean setLevel = false;
+    private boolean setLvlToggle = false;
     private int lvl;
+    private boolean lvlWait = false;
     private int liftTolerance = 100;
     RobotDrive robot = new RobotDrive();
     private int levels[] = {-500,-1800,-3400};
@@ -35,29 +37,44 @@ public class TeleOpTest extends LinearOpMode{
 
             robot.mixDrive(forward, strafe, rotate);
 
+
+            //toggle either to set the level auto or manual
+            if(gamepad2.y && !setLvlToggle) {
+                setLevel = true;
+                setLvlToggle = true;
+            } else if(gamepad2.y && !setLvlToggle) {
+                setLevel = false;
+                setLvlToggle = true;
+            }else if(!gamepad2.y)
+                setLvlToggle = false;
+
+
             //up is left trigger aka moving the motor in the negative direction
             //set the power and direction of lift motor by subtracting the two values also check to see if robot is at top or bottom
-            if (robot.liftMotor.getCurrentPosition() <= -4700 && gamepad2.right_trigger >0.1) {
+            if (robot.liftMotor.getCurrentPosition() <= -4700) {
                 robot.liftMotor.setPower(gamepad2.right_trigger);
                 setLevel = false;
-            } else if (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8 && gamepad2.left_trigger >0.1) {
+            } else if (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8) {
                 robot.liftMotor.setPower(-gamepad2.left_trigger);
                 setLevel = false;
-            } else if ((!(robot.liftMotor.getCurrentPosition() <= -4700) || (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8))
-                    && gamepad2.left_trigger >0.1 && gamepad2.right_trigger >0.1){
+            } else if ((!(robot.liftMotor.getCurrentPosition() <= -4700) || (robot.dist.getDistance(DistanceUnit.INCH) <= 1.8))){
                 robot.liftMotor.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
                 setLevel = false;
             }
 
-        if(gamepad2.left_bumper && gamepad2.right_bumper){}
-        else if(gamepad2.left_bumper && lvl < 2) {
-            setLevel = true;
+
+            //change the auto level to a diffrent one and check if it has being held down
+        if(!gamepad2.left_bumper && !gamepad2.right_bumper)
+            lvlWait = true;
+        else if(gamepad2.left_bumper && lvl < 2 && lvlWait) {
+            lvlWait = false;
             lvl++;
-        }else if(gamepad2.right_bumper && lvl > 0) {
-            setLevel = true;
+        }else if(gamepad2.right_bumper && lvl > 0 && lvlWait) {
+            lvlWait = false;
             lvl--;
         }
 
+        //check if its at the right level and stop if it is
         if(setLevel)
             if(robot.liftMotor.getCurrentPosition() < levels[lvl] && robot.liftMotor.getCurrentPosition() > levels[lvl] - liftTolerance)
                 robot.liftMotor.setPower(0);
@@ -101,10 +118,9 @@ public class TeleOpTest extends LinearOpMode{
 
         telemetry.addData("Lift yn ",setLevel);
         telemetry.addData("liftLvl: ", lvl);
-        telemetry.addData("Left Rear motor power: ",robot.motors[0].getPower());
-        telemetry.addData("GrabberServo: ", robot.Grabber.getPosition());
-        telemetry.addData("Distance: ","%.3f",((DistanceSensor) robot.dist).getDistance(DistanceUnit.INCH));
         telemetry.addData("LiftEncoder",robot.liftMotor.getCurrentPosition());
+        telemetry.addData("Distance: ","%.3f",((DistanceSensor) robot.dist).getDistance(DistanceUnit.INCH));
+
         telemetry.update();
     }
     }
