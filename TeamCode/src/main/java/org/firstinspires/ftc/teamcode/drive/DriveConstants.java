8a81c28f8b4acfc74246bd66532a9692857891d3
Owen-Pryga <58037675+Owen-Pryga@users.noreply.github.com>
2022-01-25 15:02:06 -0600

    tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,8 +55,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / 59.5929906102;
-    public static double kA = 0.000002;
+    public static double kV = 0.03504673515229;
+    public static double kA = 0.002;
     public static double kStatic = 0;
 
     /*
@@ -87,7 +87,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 59.5929906102;
+    public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 27.37808004063948;
     public static double MAX_ANG_VEL = Math.toRadians(102.8621926229508);
     public static double MAX_ANG_ACCEL = Math.toRadians(102.8621926229508);
