1b7d0af36edccaaf5bacb83fbf9a981141c4887f
Jacob0Gust <gustjac000@graftonschools.org>
2022-01-25 21:21:38 -0600

    comment of opposite of meep example

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepVisualization.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepVisualization.java
@@ -42,10 +42,45 @@ public class MeepMeepVisualization {
                                 .build()
                 );
 
+        /*     *I just comment this in to use when testing*
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(10, -70 + 15/2, Math.toRadians(90)))
+                                .forward(15/2)
+                                .turn(Math.toRadians(90))
+                                .strafeTo(new Vector2d(10,-24))
+                                .addDisplacementMarker(() -> {
+                                    //claw move and drop block
+                                })
+                                .waitSeconds(0.5)
+                                .strafeTo(new Vector2d(5,-8))
+                                .forward(20)
+                                .splineTo(new Vector2d(-45,-20),Math.toRadians(0))
+                                .back(11)
+                                .strafeTo(new Vector2d(-55,-55))
+                                .addDisplacementMarker(() -> {
+                                    // add duckwheel moving
+                                })
+                                .waitSeconds(3)
+                                .forward(1)
+                                .strafeTo(new Vector2d(-55,-20))
+                                .splineTo(new Vector2d(10,-8),Math.toRadians(0))
+                                .strafeTo(new Vector2d(10,-40))
+                                //temporary just do motorpower full for like half a sec
+                                .waitSeconds(2)
+                                .forward(140)
+                                .build()
+
+                );
+
+         */
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
+
     }
 }
