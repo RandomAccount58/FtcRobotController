29fd4614a51b366e2085ce1f786e0f0b0912a6ee
Owen-Pryga <58037675+Owen-Pryga@users.noreply.github.com>
2022-01-27 15:12:54 -0600

    did thing to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAlianceNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAlianceNear.java
@@ -15,7 +15,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 @Autonomous(name = "RedAlianceNear",group = "Auto")
 public class RedAlianceNear extends LinearOpMode {
     OpenCvCamera webcam;
-    int barcode;
+    int barcode = -1;
     @Override
     public void runOpMode() throws InterruptedException {
         //initalizing the webcam for OPENCV
@@ -42,56 +42,69 @@ public class RedAlianceNear extends LinearOpMode {
             }
         });
 
+        Thread.sleep(5000);
+
+        while(barcode == -1) {
+            switch (detector.getLocation()) {
+                case LEFT:
+                    barcode = 2;
+                    break;
+                case RIGHT:
+                    barcode = 0;
+                    break;
+                case MIDDLE:
+                    barcode = 1;
+                    break;
+                default:
+                    barcode = -1;
+                    break;
+            }
+        }
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        TrajectorySequence mainDrive = drive.trajectorySequenceBuilder(new Pose2d(-36, -70 + 15/2, Math.toRadians(90)))
+        Pose2d startPose = new Pose2d(-36, -70 + 15 / 2, Math.toRadians(90));
+
+        TrajectorySequence mainDrive = drive.trajectorySequenceBuilder(startPose)
                 .forward(15/2)
                 .turn(Math.toRadians(-90))
                 .strafeTo(new Vector2d(-36,-23))
                 .forward(barcode*2)
-                .addDisplacementMarker(() -> {
-                    robot.Grabber.setPosition(0.75);
-                })
-                .waitSeconds(0.5)
-                .back(1)
-                .splineTo(new Vector2d(-55,-55),Math.toRadians(-90))
-                .addDisplacementMarker(() -> {
-                    robot.duckMotor.setPower(0.75);
-                })
-                .waitSeconds(3)
+                .build();
+
+        TrajectorySequence secondDrive = drive.trajectorySequenceBuilder(startPose)
+                .back(barcode*2)
+                .splineTo(new Vector2d(-70 +15/2 + 2,-70 +15/2 + 2),Math.toRadians(-90))
+                .build();
+
+        TrajectorySequence thirdDrive = drive.trajectorySequenceBuilder(startPose)
                 .forward(1)
                 .splineTo(new Vector2d(-60,-36),Math.toRadians(90))
                 .build();
 
+        robot.turnOnLights();
         waitForStart();
 
-        robot.dropArm.setPosition(1);
+        while(opModeIsActive()) {
+            robot.dropArm.setPosition(1);
 
-        switch (detector.getLocation())
-        {
-            case LEFT:
-                barcode = 2;
-                break;
-            case RIGHT:
-                barcode= 0;
-                break;
-            case MIDDLE:
-                barcode = 1;
-                break;
-            default:
-                barcode = -1;
-                break;
+            webcam.stopStreaming();
+
+            telemetry.addData("Detected Level: ", barcode);
+            telemetry.update();
+            robot.turnOnLights();
+            if (barcode > -1)
+                robot.nextLevel(robot.levels[barcode]);
+
+            drive.followTrajectorySequence(mainDrive);
+            robot.Grabber.setPosition(-1);
+            Thread.sleep(500);
+            drive.followTrajectorySequence(secondDrive);
+            robot.duckMotor.setPower(0.75);
+            Thread.sleep(200);
+            robot.duckMotor.setPower(0);
+            drive.followTrajectorySequence(thirdDrive);
+            robot.liftOdoPods();
+            requestOpModeStop();
         }
-
-        webcam.stopStreaming();
-
-        telemetry.addData("Detected Level: ",barcode);
-        telemetry.update();
-        robot.turnOnLights();
-        if(barcode > -1)
-            robot.nextLevel(robot.levels[barcode]);
-
-        drive.followTrajectorySequence(mainDrive);
-        robot.liftOdoPods();
-
     }
 }
